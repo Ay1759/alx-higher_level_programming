2319169b4c238099ff394fe68c4eba29f69557f2
ayobami lasisi <ayo4las@gmail.com>
2023-08-03 13:07:26 -0700

    0x15-javascript

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -13,4 +13,15 @@ request(API_URL + episodeNum, function (err, response, body) {
   } else {
     console.log('Error code: ' + response.statusCode);
   }
-});
+})#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
+  }
+});;
